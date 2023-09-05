4cb7e70239b5daa2e97845f396821b6b86a8241b
Sklearosi <giorgi.chanturidze.2000@gmail.com>
2023-09-05 19:48:37 +0400

    feat:add and delete features

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,49 @@
-import { useState } from "react";
-
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { TodoTask } from "./types.d";
 function App() {
+  const [task, setTask] = useState<string>("");
+  const [todo, setTodo] = useState<TodoTask[]>([]);
+
+  const getData = async () => {
+    try {
+      const response = await axios.get(
+        "https://sqltodoapp.onrender.com/api/tasks"
+      );
+
+      console.log(response.data);
+      setTodo(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const insert = async () => {
+    try {
+      const response = await axios.post(
+        "https://sqltodoapp.onrender.com/api/tasks",
+        { task: task, active: true },
+        {
+          headers: {
+            Authorization: "Bearer rnd_SgkERpAX8npk0GmuBnhAywYMXR1R",
+          },
+        }
+      );
+
+      console.log(response);
+      setTodo(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+
+    setTask("");
+    getData();
+  };
+
   return (
     <div>
       <div
@@ -18,15 +61,62 @@ function App() {
           className=" ml-3 w-5 h-5 rounded-full border border-[background: linear-gradient(135deg, #55DDFF 0%, #C058F3 100%)]"
           htmlFor="for-task"
         ></label>
-        <input className=" hidden" type="checkbox" name="" id="for-task" />
+        <input
+          className=" hidden"
+          type="checkbox"
+          name=""
+          id="for-task"
+          onChange={insert}
+        />
         <input
           className=" h-5 outline-none text-[#9495A5] placeholder:text-[#9495A5] text-[12px] text-left ml-3 font-[400]"
           placeholder="Create a new todo…"
           type="text"
+          onChange={(e) => {
+            setTask(e.target.value);
+          }}
         />
       </div>
       <div className=" bg-white rounded-xl h-auto w-[327px] m-auto mt-5 shadow-shadowForFirst">
-        <ul></ul>
+        <ul>
+          {todo.map((tsk) => {
+            return (
+              <li
+                className=" h-12 m-auto flex justify-left items-center border-b border-[#E3E4F1] "
+                key={tsk.id}
+              >
+                <div className="w-[287px] m-auto flex justify-between items-center">
+                  <div className="flex items-center">
+                    <label
+                      className="w-5 h-5 rounded-full border border-[background: linear-gradient(135deg, #55DDFF 0%, #C058F3 100%)]"
+                      htmlFor={tsk.task}
+                      
+                    ></label>
+                    <input className="hidden" type="checkbox" id={tsk.task} />
+                    <p className={` ml-2 text-[400] text-[12px]  ${tsk.active ? 'text-[#494C6B]':'text-[#D1D2DA]'}`}>
+                      {tsk.task}
+                    </p>
+                  </div>
+                  <img
+                    onClick={async () => {
+                      try {
+                        const response = await axios.delete(
+                          `https://sqltodoapp.onrender.com/api/tasks/${tsk.id}`
+                        );
+                      } catch (error) {
+                        console.log(error);
+                      }
+                      getData();
+                    }}
+                    className=" w-3 h-3"
+                    src="images/icon-cross.svg"
+                    alt=""
+                  />
+                </div>
+              </li>
+            );
+          })}
+        </ul>
         <div className=" h-12 flex w-[287px] m-auto justify-between items-center text-[400] text-[12px] text-[#9495A5]">
           <p>items left</p>
           <p>Clear Completed</p>
---
+++ src/types.d.tsx
@@ -0,0 +1,7 @@
+
+
+export type TodoTask = {
+    id: number,
+    task: string,
+    active: boolean,
+}
